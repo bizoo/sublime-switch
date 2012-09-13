78347e608c044d6c1df3f7e686d92113b07dc4ba
Robert Schulze <robertschulze@gmx.net>
2012-09-13 03:01:10 +0200

    Update default switches
    * Split default switches into language specific settings.
    * Add possibility to use custom switches.
    * Add possibility to overwrite buildin switches

--- Switch.py
+++ Switch.py
@@ -1,32 +1,24 @@
 import sublime, sublime_plugin, re
 
-settings = sublime.load_settings('Switch.sublime-settings')
-
-class Pref:
-  def load(self):
-    Pref.switch_definitions = settings.get('switch_definitions')
-
-Pref = Pref()
-Pref.load()
-
-settings.add_on_change('reload', lambda:Pref.load())
-
 class SwitchCommand(sublime_plugin.TextCommand):
   def run(self, edit):
+    settings = self.view.settings()
+
+    switch_definitions = settings.get('switch_builtin_definitions')
+    if settings.has('switch_custom_definitions'):
+      switch_definitions += settings.get('switch_custom_definitions')
+
     for region in self.view.sel():
-      for extension, switches in Pref.switch_definitions.iteritems():
-        if extension == "*" or extension == self.view.file_name().split(".")[-1]:
-          if self.apply_switches_on_region(edit, switches, region): break
+      for group in switch_definitions:
+        if self.apply_switch_on_region(edit, group, region): break
 
-  def apply_switches_on_region(self, edit, switches, region):
-    word_region  = self.view.word(region)
-    line_region  = self.view.line(region)
-
-    for switch, group in switches.iteritems():
-      if isinstance(group, list):
-        if self.apply_list_on_region(edit, group, word_region): return True
-      elif isinstance(group, dict):
-        if self.apply_dict_on_region(edit, group, line_region): return True
+  def apply_switch_on_region(self, edit, group, region):
+    if isinstance(group, list):
+      word_region  = self.view.word(region)
+      if self.apply_list_on_region(edit, group, word_region): return True
+    elif isinstance(group, dict):
+      line_region  = self.view.line(region)
+      if self.apply_dict_on_region(edit, group, line_region): return True
     else: return False
 
   def apply_list_on_region(self, edit, group, region):
@@ -37,7 +29,7 @@ class SwitchCommand(sublime_plugin.TextCommand):
         next_item_index = 0 if item_index >= len(group) - 1 else item_index + 1
         self.view.replace(edit, region, group[next_item_index])
         return True
-    return False
+    else: return False
 
   def apply_dict_on_region(self, edit, group, region):
     line_content = self.view.substr(region)
